188a241200c16ad186e023e8b49735ae51181f76
bluerune <b.sofia.sosa@gmail.com>
2024-08-14 06:18:57 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
 const divBoton = document.getElementById("divBoton");
+const boton = document.getElementById("boton");
 
 divBoton.addEventListener("click", saludarDiv);
+boton.addEventListener("click", noPropagar);
 
 function saludarDiv() {
     alert("Hola! Soy el div");
+}
+
+function noPropagar(e) {
+    e.stopPropagation();
 }
